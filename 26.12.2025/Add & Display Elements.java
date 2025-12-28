e7567beaba2e318418c6ee299396b7cb1a377a8d
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:42:35 +0530

    Create Add & Display Elements.java

---
+++ 26.12.2025/Add & Display Elements.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AL1 {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("Java");
+        a.add("Python");
+        System.out.println(a);
+    }
+}
