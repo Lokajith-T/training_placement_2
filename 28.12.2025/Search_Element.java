a1a064e35067ed29d4c0f5cd77293bdfe9520afc
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:44:13 +0530

    Create Search_Element.java

---
+++ 28.12.2025/Search_Element.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class AL3 {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("A"); a.add("B");
+        System.out.println(a.contains("B"));
+    }
+}
