b5c9e20aad11ca8ef1c569b19d861044a087b159
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:44:40 +0530

    Create Sort_ArrayList.java

---
+++ 28.12.2025/Sort_ArrayList.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AL4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(3); a.add(1); a.add(2);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+}
