0270468767e2228382ef3e6345ce4b6d91956ada
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:48:29 +0530

    Create Automatic_Sorting.java

---
+++ 28.12.2025/Automatic_Sorting.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class TM1 {
+    public static void main(String[] args) {
+        TreeMap<Integer,String> t=new TreeMap<>();
+        t.put(3,"C"); t.put(1,"A");
+        System.out.println(t);
+    }
+}
