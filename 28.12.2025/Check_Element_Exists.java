c8e17158924cbf9a98418dbe7fef9db180509ad6
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:46:14 +0530

    Create Check_Element_Exists.java

---
+++ 28.12.2025/Check_Element_Exists.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class HS3 {
+    public static void main(String[] args) {
+        HashSet<Integer> s=new HashSet<>();
+        s.add(10); s.add(20);
+        System.out.println(s.contains(10));
+    }
+}
