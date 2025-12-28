508d586b0123146ab5f9a70972d6ad5565e0ecab
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:46:54 +0530

    Create Store Key-Value Pairs.java

---
+++ 28.12.2025/Store Key-Value Pairs.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class HM1 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> m=new HashMap<>();
+        m.put(1,"A"); m.put(2,"B");
+        System.out.println(m);
+    }
+}
