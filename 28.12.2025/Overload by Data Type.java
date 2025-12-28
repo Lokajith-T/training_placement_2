d00f0723dce8b0f92f108318949a9109b0dce19e
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:50:13 +0530

    Create Overload by Data Type.java

---
+++ 28.12.2025/Overload by Data Type.java
@@ -0,0 +1,8 @@
+class OL2 {
+    void show(int a){System.out.println(a);}
+    void show(String a){System.out.println(a);}
+    public static void main(String[] args) {
+        OL2 o=new OL2();
+        o.show(10); o.show("Java");
+    }
+}
