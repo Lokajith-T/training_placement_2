37a4e4ec976e09726bd7ad24ef8a2308d06f9f65
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:43:45 +0530

    Create Remove_Element.java

---
+++ 28.12.2025/Remove_Element.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AL2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(10); a.add(20);
+        a.remove(0);
+        System.out.println(a);
+    }
+}
