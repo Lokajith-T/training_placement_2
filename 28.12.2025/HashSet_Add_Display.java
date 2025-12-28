37fda75ba352ba7b42b1038a404200923eae2cc0
Lokajith-T <lokajith4@gmail.com>
2025-12-28 22:45:45 +0530

    Create HashSet_Add_Display.java

---
+++ 28.12.2025/HashSet_Add_Display.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class HS2 {
+    public static void main(String[] args) {
+        HashSet<String> s=new HashSet<>();
+        s.add("Java"); s.add("Java");
+        System.out.println(s);
+    }
+}
